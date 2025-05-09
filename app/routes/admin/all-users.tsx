2f7eea7cba881dc0450e0ee7ad6293fd347f9e47
mayurbagade456 <mayurbagade456@gmail.com>
2025-05-09 12:21:14 +0530

    Implement Routing, Dashboard, Layout &UI, Auth Functionality

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,16 @@
+import { Header } from 'components'
+import React from 'react'
+
+const AllUsers = () => {
+  return (
+    <main className="all-users wrapper">
+            <Header
+                title="Manage Users"
+                description="Filter, sort, and access detailed user profiles"
+            />
+            All Users Page content
+            </main>
+  )
+}
+
+export default AllUsers
